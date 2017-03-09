d6107105ad2049e7dd7fc313fc9bd346e0d0c689
Will Vincent <will@willvincent.com>
2017-03-09 16:05:27 -0600

    Add require for jquery to main js file.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-/* Main entry point of JS code. */
+/**
+ * ---------------------------------------------------------------
+ * Main Entry Point of Javascript
+ * ---------------------------------------------------------------
+ */
+
+/**
+ * Since jQuery is required by the framework & framework extras
+ * We're including it here by default.
+ */
+window.$ = window.jQuery = require('jquery');
